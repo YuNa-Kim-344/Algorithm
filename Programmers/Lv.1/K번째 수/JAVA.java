49d49c043efdca3e4d4824338750f7996e457097
YuNA <rladbsk2805@g.hongik.ac.kr>
2025-09-11 11:34:45 +0900

    java code

---
+++ Programmers/Lv.1/K번째 수/JAVA.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int idx = 0; idx < commands.length; idx++){
+            int i = commands[idx][0];
+            int j = commands[idx][1];
+            int k = commands[idx][2];
+            
+            int[] new_array = Arrays.copyOfRange(array, i-1, j);
+            Arrays.sort(new_array);
+            answer[idx] = new_array[k-1];
+            
+        }
+        
+        return answer;
+    }
+}

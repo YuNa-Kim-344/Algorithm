65126d5db5d006dc67da8133a65c8454d628992a
YuNA <rladbsk2805@g.hongik.ac.kr>
2026-06-05 19:43:37 +0900

    Create JAVA.java

---
+++ Programmers/Lv.2/올바른 괄호/JAVA.java
@@ -0,0 +1,30 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        if (s.charAt(0) == ')'){
+            return false;
+        }
+        
+        int a = 0;
+        
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '('){
+                a += 1;
+            }
+            else if (s.charAt(i) == ')'){
+                a+= -1;
+            }
+            
+            if (a < 0){
+                return false;
+            }
+        }
+        
+        if (a != 0){
+            return false;
+        }
+
+        return answer;
+    }
+}

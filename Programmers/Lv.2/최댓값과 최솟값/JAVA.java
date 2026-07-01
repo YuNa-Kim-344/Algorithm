3e0f81e70d30c65206c3aaeff832f17c5dc92022
YuNA <rladbsk2805@g.hongik.ac.kr>
2026-07-01 16:52:26 +0900

    Create JAVA.java

---
+++ Programmers/Lv.2/최댓값과 최솟값/JAVA.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] numbers = s.split(" ");
+        int min = Integer.parseInt(numbers[0]);
+        int max = Integer.parseInt(numbers[0]);
+
+        for (int i = 1; i < numbers.length; i++) {
+            int num = Integer.parseInt(numbers[i]);
+            if (num < min) {
+                min = num;
+            }
+
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        answer = min + " " + max;
+        
+        return answer;
+    }
+}

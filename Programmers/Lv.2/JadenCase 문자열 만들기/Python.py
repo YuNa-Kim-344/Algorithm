0889053f5ad3f4caf590b1c82f0129bb5364d987
YuNA <rladbsk2805@g.hongik.ac.kr>
2026-07-02 16:27:17 +0900

    Implement JadenCase string conversion function

---
+++ Programmers/Lv.2/JadenCase 문자열 만들기/Python.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = ''
+    
+    s = s.split(" ")
+    
+    for i in range(len(s)):
+        if s[i] != "":
+            s[i] = s[i][0].upper() + s[i][1:].lower()
+                
+    answer = " ".join(s)          
+
+    return answer

e5925373e2bdb396679f64af9f7faffc817cfb67
YuNA <rladbsk2805@g.hongik.ac.kr>
2026-07-02 15:49:33 +0900

    Implement budget allocation solution in Python

---
+++ Programmers/Lv.1/예산/Python.py
@@ -0,0 +1,13 @@
+def solution(d, budget):
+    answer = 0
+    
+    d.sort()
+    
+    for i in range(len(d)):
+        budget -= d[i]
+        if budget >= 0:
+            answer += 1
+        else:
+            break
+    
+    return answer

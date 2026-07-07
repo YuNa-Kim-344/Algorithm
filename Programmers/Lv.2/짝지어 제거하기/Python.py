9e9203b8c615043cd4613fc2a4f3f3552d5ba14a
YuNA <rladbsk2805@g.hongik.ac.kr>
2026-07-07 14:58:10 +0900

    Update Python.py

--- Programmers/Lv.2/짝지어 제거하기/Python.py
+++ Programmers/Lv.2/짝지어 제거하기/Python.py
@@ -1,15 +1,14 @@
 def solution(s):
 
-    while len(s) > 0:
-        removed = False
-        for i in range(len(s)-1):
-            if s[i] == s[i+1]:
-                s = s[:i] + s[i+2:]
-                removed = True
-                break
-            
-        if len(s) == 0:
-            return 1
-        if removed == False:
-            return 0
+    stack = []
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
             

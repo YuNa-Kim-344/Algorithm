681130f5e3f4a025db387e105de72c1cbe44d9be
YuNA <rladbsk2805@g.hongik.ac.kr>
2026-07-23 15:37:00 +0900

    Create JAVA.java

---
+++ Programmers/Lv.1/문자열 내 p와 y의 개수/JAVA.java
@@ -0,0 +1,27 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        s = s.toLowerCase();
+        
+        int p_count = 0;
+        int y_count = 0;
+        
+        for(int i=0; i < s.length(); i++){
+            if(s.charAt(i) == 'p'){
+                p_count++;
+            }
+        }
+        for(int i=0; i < s.length(); i++){
+            if(s.charAt(i) == 'y'){
+                y_count++;
+            }
+        }
+        
+        if(p_count != y_count){
+            answer = false;
+        }
+
+        return answer;
+    }
+}

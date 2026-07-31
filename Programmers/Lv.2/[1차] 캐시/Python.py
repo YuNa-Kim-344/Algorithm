6e1de93845de8b8de0e9612b6e89a6ad1bfda01c
YuNA <rladbsk2805@g.hongik.ac.kr>
2026-07-31 17:20:27 +0900

    Create Python.py

---
+++ Programmers/Lv.2/[1차] 캐시/Python.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque()
+    
+    if cacheSize == 0:
+        return len(cities) * 5
+    
+    for i in range(len(cities)):
+        city = cities[i].lower()
+        
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else:
+            if len(cache) == cacheSize:
+                cache.popleft()
+                
+            cache.append(city)
+            answer += 5
+    
+    return answer

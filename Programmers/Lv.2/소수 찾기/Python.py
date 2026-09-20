0f5d1b8175058705fd7a30da27dcc23f4c440867
YuNA <rladbsk2805@g.hongik.ac.kr>
2026-09-20 20:11:07 +0900

    Create Python.py

---
+++ Programmers/Lv.2/소수 찾기/Python.py
@@ -0,0 +1,32 @@
+def solution(numbers):
+    answer = 0
+    
+    num_list = list(numbers)
+    
+    def is_prime(n):
+        if n < 2:
+            return False
+        
+        for i in range(2, int(n ** 0.5)+1):
+            if n%i == 0:
+                return False
+        
+        return True
+    
+    made_num = set()
+    
+    def bfs(N, numbers):
+        
+        for i in range(len(numbers)):
+            new_num = N + numbers[i]
+            made_num.add(int(new_num))
+            
+            bfs(new_num, numbers[:i]+numbers[i+1:])
+        
+    bfs("", numbers)
+    
+    for i in made_num:
+        if is_prime(i):
+            answer += 1
+    
+    return answer
